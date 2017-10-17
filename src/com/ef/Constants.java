93c634346fadc2f7eeca81e3f84622cd8acbb9b4
Sasa Prsic <s.prsic@gmail.com>
2017-10-17 11:36:39 +0200

    Added mysql connector to idea lib and some idea specific for easier importing a project

--- src/com/ef/Constants.java
+++ src/com/ef/Constants.java
@@ -10,7 +10,7 @@ import java.time.format.DateTimeFormatter;
 public class Constants {
     public final static DateTimeFormatter inputDateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd.HH:mm:ss");
     public final static DateTimeFormatter logDateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS");
-    public final static String DB_PASSWORD = "piramida";
+    public final static String DB_PASSWORD = "";
     public final static String DB_USER = "root";
     public final static int BATCH_SIZE = 1000;
     public final static String LOG_FILE_PATH = "access.log";
